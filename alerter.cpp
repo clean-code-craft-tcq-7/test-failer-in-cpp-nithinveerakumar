00c3486d319435066b7ac5f363d81081eee69989
ndt1cob <dhanakumar.nithinveerakumar@in.bosch.com>
2022-11-29 19:53:05 +0530

    alerter

--- alerter.cpp
+++ alerter.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <assert.h>
-
 int alertFailureCount = 0;
+//dummy of actual production function 
+int networkAlert(float celcius){
+    std::cout << "[PROD]ALERT: Temperature is " << celcius << " celcius.\n";
+    return 0;
+}
 
 int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+    std::cout << "[STUB]ALERT: Temperature is " << celcius << " celcius.\n";
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    if(celcius>200) {
+        return 500;
+    }
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
+
+void alertInCelcius(float farenheit,bool isTestEnvironment=false) {
+    float celcius = (farenheit - 32) * 5 / 9; 
+    int returnCode;
+    if (isTestEnvironment) {
+        returnCode = networkAlertStub(celcius);
+    }
+    else {
+        returnCode=networkAlert(celcius);
+    }
+    if(returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
@@ -24,8 +38,10 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+    alertInCelcius(400.5,true);
+    assert(alertFailureCount==0);
+    alertInCelcius(303.6,true);
+    assert(alertFailureCount==1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
