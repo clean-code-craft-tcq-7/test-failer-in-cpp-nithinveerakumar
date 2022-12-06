b1661f7d797b5c82682bacb9512dd98822f3db1f
ndt1cob <dhanakumar.nithinveerakumar@in.bosch.com>
2022-12-06 16:46:18 +0530

    make the testcase pass

--- alerter.cpp
+++ alerter.cpp
@@ -19,31 +19,29 @@ int networkAlertStub(float celcius) {
 }
 
 
-void alertInCelcius(float farenheit,bool isTestEnvironment=false) {
+void alertInCelcius(float farenheit,int (*networkAlerter) (float) =&networkAlert ) {
     float celcius = (farenheit - 32) * 5 / 9; 
     int returnCode;
-    if (isTestEnvironment) {
-        returnCode = networkAlertStub(celcius);
-    }
-    else {
-        returnCode=networkAlert(celcius);
-    }
+    returnCode = networkAlerter(celcius);
     if(returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
-    alertInCelcius(400.5,true); // above threshold
+    int (*networkAlerterstub) (float) = &networkAlertStub;
+    alertInCelcius(400.5,networkAlerterstub); // above threshold
     assert(alertFailureCount==1);
-    alertInCelcius(392,true);//  threshold
+    alertInCelcius(392,networkAlerterstub);//  threshold
     assert(alertFailureCount==1);
-    alertInCelcius(303.6,true);// below threshold
+    alertInCelcius(303.6,networkAlerterstub);// below threshold
     assert(alertFailureCount==1);
+    alertInCelcius(500.5,networkAlerterstub); // above threshold
+    assert(alertFailureCount=2);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
--- misaligned.cpp
+++ misaligned.cpp
@@ -4,36 +4,64 @@
 #include <map>
 #include<cstring>
 using namespace std;
-const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ const char* majorColorArray[] = {"White", "Red", "Black", "Yellow", "Violet"};
+ const char* minorColorArray[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-map <int,string> printColorMap() {
-    map <int,string> colorMap;
+struct ColorPair{
+    const char* majorColor;
+    const char* minorColor;
+    int PairNumber;
+};
+
+map <int,ColorPair> getColourMap() {
+    map <int,ColorPair> colorMap;
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::ostringstream oss;
-            std::cout << i * 5 + j + 1 << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-            oss<< i * 5 + j + 1 << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-            cout<<oss.str();
-            colorMap.insert(pair<int,string>(i*5+j+1,oss.str()));
+            ColorPair colorPair;
+            colorPair.PairNumber = i* 5 + j + 1;
+            colorPair.majorColor = majorColorArray[i];
+            colorPair.minorColor = minorColorArray[j];
+            colorMap.insert(pair<int,ColorPair>(i*5+j+1,colorPair));
         }
     }
     return colorMap;
 }
 
+string getFormattedColorPairString(ColorPair colorPair)
+{
+    string formattedString=std::to_string(colorPair.PairNumber)+ " | " + colorPair.majorColor + " | " + colorPair.minorColor;
+    std::string s = std::to_string(colorPair.PairNumber);
+    return formattedString;    
+}
+
+void printColorPairMap()
+{   map<int, ColorPair> colorMap = getColourMap();
+    for(map<int, ColorPair>::iterator itr=colorMap.begin();itr != colorMap.end(); ++itr)
+    {
+        cout << getFormattedColorPairString(itr->second)<<endl;
+    }
+}
+
 int main() {
-    map <int,string> result = printColorMap();
-    assert(result.size() == 25);
-    int pairNumber=1; 
+    printColorPairMap();
+    map<int, ColorPair> colorMap = getColourMap();
     //test for pairNumber 1
-    assert(strcmp(result[pairNumber].c_str(),"1 | White | Blue\n")==0);
-    //test for pairNumber 2
+    int pairNumber=1;
+    assert(strcmp(colorMap[pairNumber].majorColor,"White")==0 && strcmp(colorMap[pairNumber].minorColor,"Blue")==0);
+    assert(strcmp(getFormattedColorPairString(colorMap[pairNumber]).c_str(),"1 | White | Blue/0"));
+   //test for pairNumber 2
     pairNumber=2;
-    assert(strcmp(result[pairNumber].c_str(),"2 | White | Orange\n")==0);
+    assert(strcmp(colorMap[pairNumber].majorColor,"White")==0 && strcmp(colorMap[pairNumber].minorColor,"Orange")==0);
+    assert(strcmp(getFormattedColorPairString(colorMap[pairNumber]).c_str(),"1 | White | Orange/0"));
+    //test for pairNumber 11
+    pairNumber=11;
+    assert(strcmp(colorMap[pairNumber].majorColor,"Black")==0 && strcmp(colorMap[pairNumber].minorColor,"Blue")==0);
+    assert(strcmp(getFormattedColorPairString(colorMap[pairNumber]).c_str(),"1 | Black | Blue/0"));
     //test for pairNumber 12
     pairNumber=12;
-    assert(strcmp(result[pairNumber].c_str(),"12 | Black | Orange\n")==0);
+    assert(strcmp(colorMap[pairNumber].majorColor,"Black")==0 && strcmp(colorMap[pairNumber].minorColor,"Orange")==0);
+    assert(strcmp(getFormattedColorPairString(colorMap[pairNumber]).c_str(),"1 | Black | Orange/0"));
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
