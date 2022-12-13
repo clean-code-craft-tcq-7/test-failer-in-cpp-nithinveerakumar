6331aa4339f032146e2328b289212bfbf7f684ee
ndt1cob <dhanakumar.nithinveerakumar@in.bosch.com>
2022-12-13 22:29:56 +0530

    Fixed review comments

--- alerter.cpp
+++ alerter.cpp
@@ -7,17 +7,6 @@ int networkAlert(float celcius){
     return 0;
 }
 
-int networkAlertStub(float celcius) {
-    std::cout << "[STUB]ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    if(celcius>200) {
-        return 500;
-    }
-    return 200;
-}
-
 
 void alertInCelcius(float farenheit,int (*networkAlerter) (float) =&networkAlert ) {
     float celcius = (farenheit - 32) * 5 / 9; 
@@ -32,17 +21,3 @@ void alertInCelcius(float farenheit,int (*networkAlerter) (float) =&networkAlert
     }
 }
 
-int main() {
-    int (*networkAlerterstub) (float) = &networkAlertStub;
-    alertInCelcius(400.5,networkAlerterstub); // above threshold
-    assert(alertFailureCount==1);
-    alertInCelcius(392,networkAlerterstub);//  threshold
-    assert(alertFailureCount==1);
-    alertInCelcius(303.6,networkAlerterstub);// below threshold
-    assert(alertFailureCount==1);
-    alertInCelcius(500.5,networkAlerterstub); // above threshold
-    assert(alertFailureCount=2);
-    std::cout << alertFailureCount << " alerts failed.\n";
-    std::cout << "All is well (maybe!)\n";
-    return 0;
-}
---
+++ test_alerter.cpp
@@ -0,0 +1,26 @@
+#include "alerter.cpp"
+int networkAlertStub(float celcius) {
+    std::cout << "[STUB]ALERT: Temperature is " << celcius << " celcius.\n";
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if(celcius>200) {
+        return 500;
+    }
+    return 200;
+}
+
+int main() {
+    int (*networkAlerterstub) (float) = &networkAlertStub;
+    alertInCelcius(400.5,networkAlerterstub); // above threshold
+    assert(alertFailureCount==1);
+    alertInCelcius(392,networkAlerterstub);//  threshold
+    assert(alertFailureCount==1);
+    alertInCelcius(303.6,networkAlerterstub);// below threshold
+    assert(alertFailureCount==1);
+    alertInCelcius(500.5,networkAlerterstub); // above threshold
+    assert(alertFailureCount=2);
+    std::cout << alertFailureCount << " alerts failed.\n";
+    std::cout << "All is well (maybe!)\n";
+    return 0;
+}
